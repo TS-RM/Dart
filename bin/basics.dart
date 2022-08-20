762a0afcd5cae830cb447759c98db3ae3ea8eb12
TS-RM <muharram.rashed@gmail.com>
2022-08-20 21:38:43 +0300

    operators inside classes and singleton example

--- bin/basics.dart
+++ bin/basics.dart
@@ -3,8 +3,13 @@ import 'package:basics/notes/basics.dart';
 import 'package:basics/src/singletone.dart';
 
 void main(List<String> arguments) {
-  SingleTone s1 = SingleTone();
-  SingleTone s2 = SingleTone();
-  var output = identical(s1, s2);
+  // SingleTone s1 = SingleTone();
+  // SingleTone s2 = SingleTone();
+  int p1 = 12;
+  int p2 = 13;
+  var a = Area(x: p1, y: p2);
+  var b = Area(x: p1, y: p2);
+  var output = a + b;
+
   print(output);
 }
--- lib/src/singletone.dart
+++ lib/src/singletone.dart
@@ -1,3 +1,5 @@
+import 'package:basics/notes/basics.dart';
+
 class SingleTone {
   SingleTone._privetConstructor();
   static final _instance = SingleTone._privetConstructor();
@@ -8,4 +10,18 @@ class Tables {
   Tables._privetConstructor({required String name});
   static final _name = Tables._privetConstructor(name: "Tashil");
   factory Tables() => _name;
+}
+
+class Area {
+  const Area({required this.x, required this.y});
+
+  final int x, y;
+
+  Area operator +(Area p) => Area(x: p.x + x, y: p.y);
+  Area operator -(Area p) => Area(x: p.x + x, y: y - 2);
+
+  @override
+  String toString() {
+    return "A(x):$x,B(Y):$y";
+  }
 }
