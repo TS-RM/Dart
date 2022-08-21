72c525a2539c8a5397d1aa84a23309c869fde605
TS-RM <muharram.rashed@gmail.com>
2022-08-21 20:26:40 +0300

    Clean up start

--- lib/iterable.dart
+++ lib/iterable.dart
@@ -2,14 +2,14 @@ void main(List<String> args) {
   // numbers(12);
   Iterable<int> iterable = [1, 2, 3];
   var n = iterable.elementAt(1);
-  var nn = numbers(12);
-  var x = numbers(12).first;
-  print(x);
+  // var nn = numbers(12);
+  // var x = numbers(12).first;
+  // print(x);
 }
 
-Iterable<int> numbers(int n) async* {
-  for (var i = 0; i <= n; i++) {
-    print(i.toString());
-    yield i;
-  }
-}
+// Iterable<int> numbers(int n) async* {
+//   for (var i = 0; i <= n; i++) {
+//     print(i.toString());
+//     yield i;
+//   }
+// }
--- test/basics_test.dart
+++ test/basics_test.dart
@@ -1,15 +1,10 @@
-import 'package:basics/basics.dart';
-import 'package:basics/variables.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  test('calculate', () {});
   test("test the print type", () {
     // expect(actual, matcher);
-    expect(getType(""), "");
-    expect(getType(12), 12);
+
     isTrue;
   });
 }
