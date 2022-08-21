3813ae1b6b00b86ef88739c8f908e28146b36166
TS-RM <muharram.rashed@gmail.com>
2022-08-21 15:34:11 +0300

    generic example

--- bin/basics.dart
+++ bin/basics.dart
@@ -1,15 +1,16 @@
 import 'package:basics/classes.dart';
+import 'package:basics/generic.dart';
 import 'package:basics/notes/basics.dart';
 import 'package:basics/src/singletone.dart';
 
 void main(List<String> arguments) {
-  // SingleTone s1 = SingleTone();
-  // SingleTone s2 = SingleTone();
-  int p1 = 12;
-  int p2 = 13;
-  var a = Area(x: p1, y: p2);
-  var b = Area(x: p1, y: p2);
-  var output = a + b;
+  Animals animal = const Animals(1, 2, 3);
+  Animals animal1 = Animals(1, 2, 3);
+  Animals animal2 = Animals(1, 2, 3);
+
+  const t1 = Animals(1, 2, 3);
+  const t2 = Animals(11, 23, 4);
+  var output = t1 + t2;
 
   print(output);
 }
--- lib/generic.dart
+++ lib/generic.dart
@@ -1,17 +1,27 @@
-class Animals {
+class Animals<E> {
   // ? what is generic?
   // ? its class that can take any type of values and objects
-  final int? _a;
-  final int? _b;
-  final int? _c;
+  final E? _a;
+  final E? _b;
+  final E? _c;
 
   const Animals(this._a, this._b, this._c);
-  Animals.fromList(List<int> list)
+  Animals.fromList(List<E> list)
       : _a = list.asMap().containsKey(0) ? list[0] : null,
         _b = list.asMap().containsKey(1) ? list[1] : null,
         _c = list.asMap().containsKey(2) ? list[2] : null;
 
-  int? get first => _a;
-  int? get second => _b;
-  int? get third => _c;
+  Animals<num> operator +(Animals<num> t) =>
+      Animals(t._a! + 2, t._b! + 1, t._c);
+  Animals<num> operator -(Animals<num> t) =>
+      Animals(t._a! - 2, t._b! - 1, t._c);
+
+  E? get first => _a;
+  E? get second => _b;
+  E? get third => _c;
+
+  @override
+  String toString() {
+    return "animal_a $first, Animal_b $second, Animal_c $third";
+  }
 }
